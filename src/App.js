cf1f08265df22d532b11b44a08c998f5256c928f
Avvi Singh <singh.avvi98@gmail.com>
2022-01-05 17:01:56 +1100

    getAll() returning all data from firestore

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
-import { initProducts, dataInit } from "./services/data-init";
+import { dataInit } from "./services/data-init";
+import { readAll } from "./services/firebase-utils";
 import ProductsGrid from "./Containers";
 
 function App() {
     dataInit("products");
+    readAll();
     return (
         <div className="App">
             <h1>Avvi's Gaming Setup Store</h1>
--- src/services/firebase-utils.js
+++ src/services/firebase-utils.js
@@ -1,1 +1,7 @@
 import firestore from "./firebase";
+
+export const readAll = async () => {
+    const returnedItems = await firestore.collection("products").get();
+
+    returnedItems.forEach((doc) => console.log(doc.data()));
+};
